d5adf7ce976acfd8bf30f6a25f62e337ee1514cf
Matthieu Marshall <matthieu.marshall@redslim.net>
2023-04-10 19:32:48 +0100

    it working

--- mileage/__main__.py
+++ mileage/__main__.py
@@ -1,11 +1,11 @@
-import dash
 from dash import dcc
 from dash import html
-import plotly.express as px
+from dash import Dash
+from dash.dependencies import Output, Input, State
 import plotly.graph_objects as go
 import logging
-import pandas as pd
-import io
+from pandas import read_csv, to_datetime
+from io import StringIO
 import base64
 
 logging.basicConfig(filename="mileage.log", level=logging.DEBUG)
@@ -17,7 +17,7 @@ dropdown_options = [
 
 logging.info(f"dropdown_options are {dropdown_options}")
 
-app = dash.Dash(__name__)
+app = Dash(__name__)
 
 app.layout = html.Div(
     children=[
@@ -47,16 +47,16 @@ app.layout = html.Div(
 )
 
 @app.callback(
-    dash.dependencies.Output("mileage-graph", "figure"),
-    [dash.dependencies.Input("units", "value"), dash.dependencies.Input("upload-data", "contents")],
-    dash.dependencies.State("upload-data", "filename")
+    Output("mileage-graph", "figure"),
+    [Input("units", "value"), Input("upload-data", "contents")],
+    State("upload-data", "filename")
 )
 def define_figure(units, contents, _):
 
     if contents:
         _, content_string = contents.split(',')
         decoded = base64.b64decode(content_string)
-        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        df = read_csv(StringIO(decoded.decode('utf-8')))
         if "kilometres" in df.columns and "miles" in df.columns:
             pass
         elif "kilometres" in df.columns:
@@ -66,27 +66,30 @@ def define_figure(units, contents, _):
         else:
             raise Exception(f"Neither kilometres or miles found in uploaded file. Columns are {df.columns}")
     else:
-        df = pd.read_csv("./data/mileage.csv")
+        df = read_csv("./data/mileage.csv")
         df["miles"] = df["kilometres"] * 0.62137119
 
     if "Date" not in df.columns:
         raise Exception(f"Date not found in df.columns. Columns are {df.columns}")
 
-    df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y")
+    df["Date"] = to_datetime(df["Date"], format="%d/%m/%Y")
     df.set_index("Date", inplace=True)
     df.fillna(0, inplace=True)
 
-    fig = px.line(df, x=df.index, y=units)
-
+    units_name = {"miles": "m", "kilometres": "km"}[units]
     rolling_avg = df[units].rolling(window=7).mean()
+    data = [
+        go.Scatter(x=df.index, y=df[units], name=f"Distance per day ({units_name})", line={"width":0.5}),
+        go.Scatter(x=df.index, y=rolling_avg, name=f"7 day moving average ({units_name})", line={"width":3})
+    ]
 
-    fig.add_trace(go.Scatter(x=df.index, y=rolling_avg, name="7 day moving average"))
+    layout = go.Layout(title="Distance plot", legend={"x":1, "y":1})
+    fig = go.Figure(data=data, layout=layout)
 
-    units = {"miles": "m", "kilometres": "km"}[units]
     # figure formatting
-    fig.update_yaxes(title=f"Distance per day ({units})")
+    fig.update_yaxes(title=f"Distance per day ({units_name})")
     fig.update_xaxes(title="Date")
     return fig
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server()
